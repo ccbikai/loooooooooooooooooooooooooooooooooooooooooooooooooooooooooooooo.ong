2ad96a1816fa5b5c99470c246a71bc058c0a9975
ccbikai <ccbikai@qq.com>
2024-06-01 18:59:45 +0800

    feat: disable server redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,4 @@
-import cloudflare from "@astrojs/cloudflare";
-import netlify from "@astrojs/netlify";
-import node from "@astrojs/node";
-import vercel from "@astrojs/vercel/serverless";
-import zeabur from "@zeabur/astro-adapter/serverless";
 import { defineConfig } from "astro/config";
-import { provider } from "std-env";
-
-const adapters = {
-	cloudflare_pages: cloudflare(),
-	netlify: netlify(),
-	vercel: vercel(),
-	zeabur: zeabur(),
-	node: node({
-		mode: "standalone",
-	}),
-};
 
 // https://astro.build/config
-export default defineConfig({
-	adapter: adapters[provider] ?? adapters.node,
-	output: "hybrid",
-});
+export default defineConfig({});
